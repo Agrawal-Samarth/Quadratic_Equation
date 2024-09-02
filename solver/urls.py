9d97e924960d1bde443b7c99544f57e71107c9c9
Agrawal-Samarth <agrawalsamarth1803@gmail.com>
2024-09-02 20:41:36 +0530

    Analytics URL routing

--- solver/urls.py
+++ solver/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import analytics_views
 
 app_name = 'solver'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', views.QuadraticSolverView.as_view(), name='index'),
     path('history/', views.equation_history, name='history'),
     path('equation/<int:equation_id>/', views.equation_detail, name='detail'),
+    
+    # Analytics URLs
+    path('analytics/', analytics_views.analytics_dashboard, name='analytics'),
 ]
